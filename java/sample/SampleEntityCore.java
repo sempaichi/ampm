38b50592dde1d7db1d3ddb50cc666bee536af5c0
sempaichi <usuzaki@Usuzaki-MacBook-Air.local>
2015-10-06 23:32:44 +0900

    はじめてコミットします

--- java/sample/SampleEntityCore.java
+++ java/sample/SampleEntityCore.java
@@ -10,7 +10,7 @@ import cpw.mods.fml.relauncher.Side;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.world.biome.BiomeGenBase;
 
-
+//みるくぷろていん
 @Mod(modid = "SampleEntity", name = "SampleEntity", version = "1.0.0")
 public class SampleEntityCore {
     @EventHandler
